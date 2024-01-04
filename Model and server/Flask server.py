4c335be19d802d59cb2ec1bfaba33cf00f2b8598
ziad11222 <ziadmohnd@yahoo.com>
2024-01-04 13:23:48 +0200

    website

--- Model and server/Flask server.py
+++ Model and server/Flask server.py
@@ -18,8 +18,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DETECTED_FOLDER'] = DETECTED_FOLDER
 
 #load model
-trained_model_path = "face_detection_model.joblib"
-trained_encoder_path = "face_encoder_labels.joblib"
+trained_model_path = "D:\\Computer Vision\\face_detection_model.joblib"
+trained_encoder_path = "D:\\Computer Vision\\face_encoder_labels.joblib"
 
 face_encodings = joblib.load(trained_model_path)
 labels = joblib.load(trained_encoder_path)
